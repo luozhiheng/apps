78b3d9d2efd910476527b960a27a5ae6d8345c2f
luozhiheng <651716183@qq.com>
2024-01-20 15:31:06 +0800

    init

--- apps/search/src/pages/api/register.ts
+++
@@ -1,5 +0,0 @@
-import { createAppRegisterHandler } from "@saleor/app-sdk/handlers/next";
-import { saleorApp } from "../../../saleor-app";
-import { withOtel } from "@saleor/apps-otel";
-
-export default withOtel(createAppRegisterHandler(saleorApp), "api/register");
